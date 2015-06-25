8b8c931e42d147f0cdaed9ec8891e03681ed0127
dpouchon <david.pouchon@batiactugroupe.com>
2015-06-25 11:05:03 +0200

    version optimisée

--- scripts/controleurs/realpref.js
+++
@@ -1,9 +0,0 @@
-app.controller('RealPrefCtrl',  function ($scope, $location){
-    var realisations_preferees = JSON.parse(localStorage.getItem("realisations_preferees"));
-
-    if(realisations_preferees == null){
-        realisations_preferees = {};
-    }
-    $scope.realisations_preferees = realisations_preferees;
-    console.log($scope.realisations_preferees)      
-});
